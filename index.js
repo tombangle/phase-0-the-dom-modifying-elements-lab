b9f2cd1d0835fc9746101923b18d3f10097a3fec
Tom Bangle <tombangle@gmail.com>
2024-05-05 15:35:11 -0700

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-// Write your code here!
+const mainElement = document.querySelector(`main#main`);
+if (mainElement) {
+    mainElement.remove();
+}
+
+const newHeader = document.createElement(`h1`);
+newHeader.id = `victory`;
+newHeader.innerHTML = `Tom Bangle is the champion`;
